c40c2a90d877233c238cd42f74a3aa4e119256c0
Zero <zero.js.dev@gmail.com>
2024-01-31 16:32:50 +0330

    Try publish first npm

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     sourcemap: true,
     emptyOutDir: false,
     lib: {
-      name: 'iot-widgets',
+      name: '@gadaman-rm/iot-widgets',
       entry: [
         path.resolve(__dirname, "./src/widgets/index.ts"),
         path.resolve(__dirname, "./src/math/math.ts"),
