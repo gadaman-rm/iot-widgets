e6ff6a0c6cefbd639ae2a360772c41a52c83e688
Zero <zero.js.dev@gmail.com>
2024-03-13 17:29:00 +0330

    Add isToWidgets

--- src/widgets/utility.ts
+++ src/widgets/utility.ts
@@ -2,10 +2,17 @@ import { Gauge, Slider } from "."
 
 export type IWidgets = Slider | Gauge 
 export type IWidgetsNames = 'slider' | 'gauge' 
-
 export const strToWidgets = (widgetName: IWidgetsNames | undefined | null) => {
     switch (widgetName) {
         case 'slider': return new Slider()
         case 'gauge': return new Gauge()
+    }
+}
+
+export type IWidgetsIsNames = 'g-slider' | 'g-gauge' 
+export const isToWidgets = (widgetName: IWidgetsIsNames | undefined | null) => {
+    switch (widgetName) {
+        case 'g-slider': return new Slider()
+        case 'g-gauge': return new Gauge()
     }
 }
